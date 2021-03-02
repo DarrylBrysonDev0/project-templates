b3dbaa381ead0fb44b25f356d2ca94f45a554f44
DarrylBryson <62783148+DarrylBrysonDev0@users.noreply.github.com>
2021-03-02 03:19:44 -0500

    complete ms sql python interface

--- Docker/Python/python-mssql-comm/db-interface-test.py
+++ Docker/Python/python-mssql-comm/db-interface-test.py
@@ -20,9 +20,31 @@ def main():
         with db_interface as db_conn:
             print(' [+] Connected to database')
 
-            col = ['ID','val']
-            qStr = 'SELECT [ID],[val] FROM [telemDataStore].[dbo].[test_tbl]'
-            df = db_interface.select_query(qStr,col)
+            # col = ['ID','val']
+            # qStr = 'SELECT [ID],[val] FROM [telemDataStore].[dbo].[test_tbl]'
+            # df = db_interface.select_query(qStr,col)
+
+            # dummy df
+            d = {'ID': [1, 2], 'val': ['Test-30', 'Test-40']}
+            df = pd.DataFrame(data=d)
+            # Set dataframe property
+            db_interface.set_df(df,'[telemDataStore].[dbo].[test_tbl]')
+            # Select top 10 records
+            sel_df = db_interface.select_db_table(10)
+            print(sel_df)
+            print()
+            # Sub-select to avoid identity field
+            db_interface.set_subselect_cols(['val'])
+            sel_df = db_interface.select_db_table(10)
+            print(sel_df)
+            print()
+
+            # Write dataframe to linked database table
+            row_cnt = db_interface.insert_dataframe()
+            print(row_cnt)
+            print()
+            sel_df = db_interface.select_db_table(10)
+            print(sel_df)
 
             print(df)
         time.sleep(frq)
