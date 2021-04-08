9213b1a7ff4af58e8e4d16e396d951cce4b9152e
DarrylBrysonDev0 <62783148+DarrylBrysonDev0@users.noreply.github.com>
2021-04-08 10:19:25 -0400

    clean base structure

---
+++ Docker/Python/python-app-loop/app/python-app-loop.py
@@ -0,0 +1,139 @@
+import os
+import traceback
+import time
+from datetime import datetime
+import uuid
+import sys
+
+from microsrv_interface.comm_interface import *
+
+def set_env_param(self, paramName,defaultStr):
+    param = os.getenv(paramName)
+    res = defaultStr if not param else param
+    return res
+
+def file_name_publisher():
+    # Set sftp interface
+    sftp_interface = sftp_CONN()
+    # Set queue interface
+    rbt_interface = queue_CONN()
+
+    source_directory = set_env_param('SOURCE_PATH','/src')
+    try:
+        # Connect to SFTP server
+        print(' [*] Connecting to SFTP server')
+        with sftp_interface as sftp:
+            print(' [+] Connected to SFTP server')
+            # Connect to RabbitMQ server
+            print(' [*] Connecting to RabbitMQ server')
+            with rbt_interface as rbt_params:
+                print(' [+] Connected to RabbitMQ')
+                # List files in dir
+                file_list = sftp_interface.get_dir_list(sftp, source_directory)
+                # For each file name publish as message to output
+                for file_name in file_list:
+                    rbt_interface.write_output(file_name)
+        return
+    except Exception as err:
+    print()
+    print("An error occurred while publishing file names to queues")
+    print(str(err))
+    traceback.print_tb(err.__traceback__)
+    return
+def file_name_consumer():
+    # Set sftp interface
+    sftp_interface = sftp_CONN()
+    # Set queue interface
+    rbt_interface = queue_CONN()
+
+    source_queue = set_env_param('COMM_QUEUE','file_list')
+    try:
+        # Connect to SFTP server
+        print(' [*] Connecting to SFTP server')
+        with sftp_interface as sftp:
+            print(' [+] Connected to SFTP server')
+            # Connect to RabbitMQ server
+            print(' [*] Connecting to RabbitMQ server')
+            with rbt_interface as rbt_params:
+                print(' [+] Connected to RabbitMQ')
+                # List files in dir
+                file_list = sftp_interface.get_dir_list(sftp, source_directory)
+                # For each file name publish as message to output
+                for file_name in file_list:
+                    rbt_interface.write_output(file_name)
+        return
+    except Exception as err:
+    print()
+    print("An error occurred while publishing file names to queues")
+    print(str(err))
+    traceback.print_tb(err.__traceback__)
+    return
+def main():
+    # Set sftp interface
+    sftp_interface = sftp_CONN()
+    # Set queue interface
+    rbt_interface = queue_CONN()
+
+    # Get frequency of app container
+    frq = int(set_env_param('FREQUENCY_SEC','300'))
+    # Get Application name
+    app_name = set_env_param('APP_NAME',str(os.uname()[1]))
+
+    success_msg = ''
+    fault_msg = ''
+    # Main process thread error handling
+    try:
+        # Connect to SFTP server
+        print(' [*] Connecting to SFTP server')
+        with sftp_interface as sftp:
+            print(' [+] Connected to SFTP server')
+            # Connect to RabbitMQ server
+            print(' [*] Connecting to RabbitMQ server')
+            with rbt_interface as rbt_params:
+                print(' [+] Connected to RabbitMQ')
+                # Set input calback function
+                def input_callback(ch, method, properties, msg):
+                    print(" [+] Received %r" % msg)
+                    try:
+                        # Do something cool
+                        print(msg)
+                        '''
+                            <! Something cool>
+                        '''
+
+                        # Ack message proc completion
+                        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+                        # Register progress
+                        # msg_cnt +=1
+                        rbt_interface.write_status(str(datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
+                    except Exception as err:
+                        print(' [!] Error executing input stream {0}.'.format(app_name))
+                        # Report failure to fault channel
+                        fault_msg = app_name + ' | Timestamp:' + datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+                        rbt_interface.write_fault(fault_msg)
+                # Register callback function and start input stream
+                rbt_interface.set_input_function(input_callback)
+                rbt_interface.start_input_stream()
+                # Clean up connections
+                # rbt_interface.close_all_connections()
+        time.sleep(frq)
+        # Report success
+        success_msg = app_name + ' | Timestamp:' + datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        rbt_interface.write_success(success_msg)
+    except Exception as err:
+        print()
+        print("An error occurred while executing main proc.")
+        print(str(err))
+        traceback.print_tb(err.__traceback__)
+    return
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # project-templates
-Collection of templates for various solution 
+
+## Setup Instructions
+1. Create test docker network
+```bash
+    docker network create microservice-network
+```
+2. Deploy shared hosting services
+```bash
+    docker-compose -f "Docker/test-hosts/docker-compose.test-interface-hosts.yml" -d --build
+```
+3. 
